668b5c0c225f0c8aecde5554b266e3a083342a82
Connor Sullivan <sully4792@gmail.com>
2017-02-12 22:08:22 -0500

    version 0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 
-version = '0.0.2'
+version = '0.0.3'
 requires = ['google-api-python-client>=1.5.3']
 setup_requirements = ['pytest-runner>=2.0']
 test_requirements = ['pytest>=2.8.0']
